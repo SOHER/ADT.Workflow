232690b7842cedcd931aea0d291ff91710a26220
SOHER <emile.soher@gmail.com>
2013-12-03 15:53:21 +0100

    CRUD_TEST

--- ADT.Workflow.UI.Web.Controller/Properties/AssemblyInfo.cs
+++ ADT.Workflow.UI.Web.Controller/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("ADT.Workflow.UI.Web.Controller")]
+[assembly: AssemblyTitle("ADT.Workflow.Web.Controllers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ADT.Workflow.UI.Web.Controller")]
+[assembly: AssemblyProduct("ADT.Workflow.Web.Controllers")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
