cd031e16d34ac026247fd25f08a80521a4e6c2d5
SOHER <emile.soher@gmail.com>
2013-12-04 14:45:33 +0100

    Ajout Article CRUD

---
+++ ADT.Workflow.UI.Web.Controller/Article/ArticleController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Web;
+using System.Linq;
+using System.Transactions;
+using System.Web.Security;
+using System.Web.Mvc;
+using Microsoft.Web.WebPages.OAuth;
+using WebMatrix.WebData;
+using ADT.Workflow.Web.Models;
+
+namespace ADT.Workflow.Web.Controllers 
+{
+    public class ArticleController : Controller
+    {
+        private workflowEntities db = new workflowEntities();
+
+        //
+        // GET: /Article/
+
+        public ActionResult Index()
+        {
+            return View(db.ARTICLE.ToList());
+        }
+
+        //
+        // GET: /Article/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            ARTICLE article = db.ARTICLE.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        //
+        // GET: /Article/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Article/Create
+
+        [HttpPost]
+        public ActionResult Create(ARTICLE article)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ARTICLE.Add(article);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(article);
+        }
+
+        //
+        // GET: /Article/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            ARTICLE article = db.ARTICLE.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        //
+        // POST: /Article/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(ARTICLE article)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(article).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(article);
+        }
+
+        //
+        // GET: /Article/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            ARTICLE article = db.ARTICLE.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        //
+        // POST: /Article/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ARTICLE article = db.ARTICLE.Find(id);
+            db.ARTICLE.Remove(article);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
